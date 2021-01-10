e16d3d59eac1d4a5af7bf374c8dd5bf6129dae38
orion222 <56134554+orion222@users.noreply.github.com>
2021-01-10 10:53:42 -0500

    Update Stacker.py

--- Comp Sci Gr 10/Assignments/Stacker.py
+++ Comp Sci Gr 10/Assignments/Stacker.py
@@ -1,7 +1,8 @@
-from graphics import *
-from random import randint
+from Graphics.graphics import *
 import winsound
-from playsound import playsound
+from random import randint
+import threading
+import time
 
 gameScreen = GraphWin("window", 900, 900, autoflush=False)
 
@@ -10,11 +11,6 @@ blocks = [
 "images/2block.gif",
 "images/1block.gif"
 ]
-
-
-# 75 x 75
-hotkey = "Space"
-
 
 base = Line(Point(188, 825), Point(713, 825))
 border2 = Line(Point(188, 75), Point(188, 825))
@@ -37,32 +33,72 @@ for i in range(9):
     horizontals.append(s)
     s.draw(gameScreen)
 
-update()
-
-playing = True
 
 sizes = [112, 75, 37]
+hotkey = "space"
+level = 0
+n = 0
+block = blocks[n]
+starter = "left"
+position = 0
+size = sizes[n]
+placing = True
+stacking = True
+direction = 1
 
-if playing:
-    stacking = True
-    while stacking:
-        level = 0
-        n = 0
-        block = blocks[n]
-        placing = True
-        position = "left"
-        size = sizes[n]
-        while placing:
-            if position == "left":
+
+class classicMode (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        global position
+        global hotkey
+        global level
+        global size
+        global block
+        global stacking
+        global placing
+        global starter
+        global direction
+        while stacking:
+            if starter == "left":
                 position = 188 + size
-            elif position == "right":
+                direction = 1
+            elif starter == "right":
                 position = 188 + 525 - size
-            block = Image(Point(position, 788 - level * 75), block)
-            block.draw(gameScreen)
-            direction = 1
-            while True:
-                time.sleep(0.5 - 0.05 * level)
-                block.move(75, 0)
+                direction = -1
 
-                
-            
+            drawnblock = Image(Point(position, 788 - level * 75), block)
+            drawnblock.draw(gameScreen)
+
+            placing = True
+            update()
+            draw = True
+            while placing:
+                end = time.time() + 0.75 - 0.05 * level
+                while time.time() < end:
+                    key = gameScreen.checkKey()
+                    if key == hotkey:
+                        placing = False
+                        stacking = False
+                        draw = False
+                        if starter == "left":
+                            starter = "right"
+                        else:
+                            starter = "left"
+                        level += 1
+                        print(level)
+                        stacking = True
+                if draw:
+                    drawnblock.move(75 * direction, 0)
+                    position += 75 * direction
+                    if position - (size + 1) <= 188 or position + size + 1 >= 713:  # + 1 because its 1 pixel off
+                        direction *= -1
+
+
+thread1 = classicMode()
+thread1.start()
+
+gameScreen.mainloop()
+
